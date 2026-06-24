340899aca89b87f0006e3abfd93731fcfe23ed30
Waranya36 <waranyaearn059@gmail.com>
2026-06-24 13:27:30 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // เขียน JavaScript สำหรับทำงานบนเว็บไซค์ (Optional)
 document.addEventListener('DOMContentLoaded', function () {
-    console.log('Portfolio ของ SOMCHAI พร้อมแล้ว!');
+    console.log('Portfolio ของ WARANYA พร้อมแล้ว!');
 });
 
